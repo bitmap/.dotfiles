571d71d79c79a7029a2a9e0c30583663851d6a31
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2020-12-16 10:42:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if [ -d $HOME/.dotfiles ]; then
+  echo "$HOME/.dotfiles directory already exists" && exit 1
+fi
+
+dotfiles() {
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+touch $HOME/.hushlogin
+
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bitmap/dotfiles.git .tmpdotfiles
 rsync --recursive --verbose --exclude={'.git','README','install.sh'} .tmpdotfiles/ $HOME/
 rm -r .tmpdotfiles
+
+dotfiles update-index --assume-unchanged README
+dotfiles update-index --assume-unchanged install.sh
