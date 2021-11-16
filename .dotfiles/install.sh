23b61eef163388fde46a54bd780efabf0d1a96da
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-11-16 09:23:58 -0800

    Fix exclude option

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -11,5 +11,5 @@ dotfiles() {
 touch $HOME/.hushlogin
 
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bitmap/dotfiles.git .tmpdotfiles
-rsync --recursive --verbose --exclude={'.git'} .tmpdotfiles/ $HOME/
+rsync --recursive --verbose --exclude='.git' .tmpdotfiles/ $HOME/
 rm -r .tmpdotfiles
