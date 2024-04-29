cbf609c7ba4bf295cb7a29206adc62b4be26433b
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2024-04-29 11:49:04 -0400

    Update macos bootstrap

--- .dotfiles/macos/bootstrap.sh
+++ .dotfiles/macos/bootstrap.sh
@@ -14,27 +14,22 @@ print -P "%F{3}\nInstalling Xcode command line tools...%f"
 xcode-select --install
 
 print -P "%F{3}\nInstalling Homebrew...%f"
-echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 print -P "%F{3}\nInstalling Apps and dev tools via Homebrew...%f"
 brew bundle --file $HOME/.dotfiles/macos/Brewfile
 
-echo "%F{3}\nInstalling asdf plugins...%f"
-. $(brew --prefix asdf)/libexec/asdf.sh
-asdf plugin add deno
-asdf plugin add golang
-asdf plugin add nodejs
-asdf plugin add python
-asdf plugin add ruby
-asdf plugin add solidity
+echo "%F{3}\nInstalling mise...%f"
+curl https://mise.run | sh
+eval "$($HOME/.local/bin/mise activate zsh)"
 
-echo "%F{3}\nInstalling dev tools via asdf...%f"
-asdf install
+echo "%F{3}\nInstalling dev tools via mise...%f"
+eval "$($HOME/.local/bin/mise install)"
 
 echo "%F{3}\nInstalling rustc...%f"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 print -P "%F{3}\nSetting macOS Preferences...%f"
 source $HOME/.dotfiles/macos/defaults.sh
