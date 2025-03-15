bdbd4e4c49779bb73da4385d26751f2b72ce87d9
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2025-03-15 14:00:47 -0400

    Update URL

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -10,6 +10,6 @@ dotfiles() {
 
 touch $HOME/.hushlogin
 
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bitmap/dotfiles.git .tmpdotfiles
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bitmap/.dotfiles.git .tmpdotfiles
 rsync --recursive --verbose --exclude '.git' .tmpdotfiles/ $HOME/
 rm -r .tmpdotfiles
