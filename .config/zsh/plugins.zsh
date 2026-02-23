d0459198f5f2d2313704c213c9067cb19595d4aa
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-02-23 11:49:34 -0500

    Cleanup plugins file

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# Determine plugin directory based on OS
 case $(uname) in
 	Darwin)
 		ZSH_PLUGINS_DIR="$(brew --prefix)/share"
@@ -7,18 +8,23 @@ case $(uname) in
 	Linux)
 		ZSH_PLUGINS_DIR="/usr/share"
 		;;
+	*)
+		ZSH_PLUGINS_DIR="$HOME/.local/share"
+		;;
 esac
 
+# Define plugins to load
 ZSH_PLUGINS=(
 	"$ZSH_PLUGINS_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 )
 
-for plugin in $ZSH_PLUGINS; do
-	[[ -f $plugin ]] && source $plugin;
+# Load plugins
+for plugin in "${ZSH_PLUGINS[@]}"; do
+	[[ -f "$plugin" ]] && source "$plugin"
 done
 
+# Configure syntax highlighting if loaded
 if [[ -v ZSH_HIGHLIGHT_STYLES ]]; then
-	# disable underline
 	ZSH_HIGHLIGHT_STYLES[path]=fg=white,none
 	ZSH_HIGHLIGHT_STYLES[path_prefix]=fg=white,none
 	ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red,none
