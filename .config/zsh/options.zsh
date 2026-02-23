bd2d1aa56532ec98bc4a1ba4f2a50668cb26dee2
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-02-23 13:54:11 -0500

    (zsh) Remove bold from ls output

--- .config/zsh/options.zsh
+++ .config/zsh/options.zsh
@@ -11,6 +11,12 @@ setopt numericglobsort # sort numerically
 setopt inc_append_history # appends every command to history immediately
 setopt share_history # share history between all sessions
 
+# ls colors - remove bold from directories
+# GNU ls (gls)
+export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=32:bd=34:cd=34:su=31:sg=31:tw=34:ow=34"
+# BSD ls (macOS default)
+export LSCOLORS="exfxcxdxbxegedabagacad"
+
 # autocomplete
 autoload -Uz compinit && compinit -i
 zstyle ':completion:*' menu yes select
