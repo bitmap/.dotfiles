19af35a68719e448058ec1aa4a73c38bff789e80
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-03-06 15:55:13 -0500

    Add zsh-autosuggestions plugin

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -15,6 +15,7 @@ esac
 
 # Define plugins to load
 ZSH_PLUGINS=(
+	"$ZSH_PLUGINS_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
 	"$ZSH_PLUGINS_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 )
 
@@ -23,9 +24,16 @@ for plugin in "${ZSH_PLUGINS[@]}"; do
 	[[ -f "$plugin" ]] && source "$plugin"
 done
 
+# Configure autosuggestions if loaded
+if [[ -v ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE ]]; then
+	ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+	ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+fi
+
 # Configure syntax highlighting if loaded
 if [[ -v ZSH_HIGHLIGHT_STYLES ]]; then
 	ZSH_HIGHLIGHT_STYLES[path]=fg=white,none
 	ZSH_HIGHLIGHT_STYLES[path_prefix]=fg=white,none
 	ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red,none
 fi
+
